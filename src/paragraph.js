59a6b67f4afaf59ab52e0969f9afd4c4e12f19c8
wellington bastos <wellingtonbastos@hotmail.com>
2019-06-26 22:09:26 -0300

    1

---
+++ src/paragraph.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export const Paragraph = (props) => (
+    <p>{props.name}</p>
+
+)
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux';
+import {clickReducer} from './clickReducer';
+
+export const Reducers = combineReducers({
+    clickState: clickReducer
+
+    
+})
